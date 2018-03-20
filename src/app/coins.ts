d09a13055bc9916c406d85b4dc00580d4fdcb5fc
Paweł Lorencowicz <p.lorencowicz@gmail.com>
2018-03-20 22:33:39 +0100

    src added

---
+++ src/app/coins.ts
@@ -0,0 +1,14 @@
+export interface Coins {
+    cap24hrChange: number;
+    long: string;
+    mktcap: number;
+    perc: number;
+    price: number;
+    shapeshift: boolean;
+    short: string;
+    supply: number;
+    usdVolume: number;
+    volume: number;
+    vwapData: number;
+    vwapDataBTC: number;
+}
